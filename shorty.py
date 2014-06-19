aa9dcc7ea4d7f610ea8b972d257aa36bae319056
Eric Stein <toba@des.truct.org>
2014-06-19 18:57:40 -0500

    use caching and stop sending automagically.

--- shorty.py
+++ shorty.py
@@ -3,13 +3,16 @@
 import sys
 import irclib
 import shortyhelpers
+import time
 
 class Shorty(irclib.SimpleIRCClient) :
-	def __init__(self, server, nick, chan, len=40) :
+	def __init__(self, server, nick, chan, len=40, automatic=False) :
 		self.len = len
 		irclib.SimpleIRCClient.__init__(self)
 		self.connect(server, 6667, nick)
 		self.connection.join(chan)
+		self.short_cache = (None, 0.0)
+		self.automatic = automatic
 	
 	def on_join(self, c, e) :
 		pass
@@ -18,6 +21,13 @@ class Shorty(irclib.SimpleIRCClient) :
 		nick = e.source().split("!")[0]
 		chan = e.target()
 		txt = e.arguments()[0]
+
+		if txt.startswith('!short') :
+			text, when = self.short_cache
+			if text :
+				self.connection.privmsg(chan, "%d seconds ago: %s" % (int(time.time() - when), text))
+			return
+		
 		n = 0
 		msg = "%s's link%s:" % (nick, '%s')
 		for short in shortyhelpers.short(txt, self.len) :
@@ -30,7 +40,9 @@ class Shorty(irclib.SimpleIRCClient) :
 				plur = 's are'
 			msg = msg % plur
 
-			self.connection.privmsg(chan, msg)
+			self.short_cache = (msg, time.time())
+			if self.automatic :
+				self.connection.privmsg(chan, msg)
 
 if __name__ == '__main__' :
 	try :
